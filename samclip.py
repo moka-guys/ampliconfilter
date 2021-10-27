95445017da641e301f50324a51afee3767cd7129
David Brawand <dbrawand@gmail.com>
2021-10-27 14:58:52 +0100

    BQ masking only for outermost primer matches (softclips)

--- samclip.py
+++ samclip.py
@@ -1,49 +1,73 @@
-#⁄/usr/bin/env python
-
-import re
-import sys
-import subprocess
-import pysam
-import argparse
-from IO import bamIO
-from functools import reduce
+#!/usr/bin/env python
 
 """
-Clips primers with samtools primerclip and then reduces clipped primers BQ to avoid realignment/variant calling.
+Clips primers with samtools primerclip and then reduces
+clipped primers' BQ to avoid realignment/variant calling.
 
 Usage:
     samclip.py primers.bed alignments.bam 
 
 """
 
+import re
+import sys
+import subprocess
+import argparse
+import pysam
+from IO import bamIO
+
 # character to softclipped primers with
 QUAL_MASK = '!'
 # regex to find query consumin operations in CIGAR string
 QUERY_CONSUMERS = re.compile(r'(\d+)([MIS=X])')
 
-
-def mask_sequence(aln):
+def mask_sequence(aln, rstart, rend):
     """Masks quality according to changed softclips in cigarstring
 
     Args:
-        aln (pysam.AlignedSegment]): aligned read
+        aln (pysam.AlignedSegment): aligned read
+        rstart (int): start of non-primer sequence
+        rend (int): end of non-primer sequence
 
     Returns:
-        aln (pysam.AlignedSegment]): aligned read with modified BQ in softclipped primers
+        aln (pysam.AlignedSegment): aligned read with modified BQ in softclipped primers
     """
-    try:
-        old_cigar = aln.get_tag('OA').split(',')[3]
-    except Exception:
-        return aln
-    new_cigar = reduce(lambda a,x: a+x[1]*int(x[0]), QUERY_CONSUMERS.findall(aln.cigarstring),'')
-    old_cigar = reduce(lambda a,x: a+x[1]*int(x[0]), QUERY_CONSUMERS.findall(old_cigar),'')
-    # build new base quality string and mask position which are newly softmasked
-    new_qual = ''
-    for i,q in enumerate(aln.qual):
-        new_qual += QUAL_MASK if old_cigar[i] != new_cigar[i] and new_cigar[i] == 'S' else q
-    # assign new quality string
-    aln.qual = new_qual
+    # get query CIGAR operations
+    query_ops = QUERY_CONSUMERS.findall(aln.cigarstring)
+
+    # determine if either end is trimmed by the expected primer pair
+    mask_left = rstart == aln.reference_start
+    mask_right = aln.reference_end == rend
+
+    # mask BQ if is an expected soft-clipped primer
+    if mask_left and query_ops[0][1] == 'S':
+        mask_len = int(query_ops[0][0])
+        aln.qual = QUAL_MASK*mask_len + aln.qual[mask_len:]
+    if mask_right and query_ops[-1][1] == 'S':
+        mask_len = int(query_ops[-1][0])
+        aln.qual = aln.qual[:-mask_len] + QUAL_MASK*mask_len
+
     return aln
+
+
+def resolve(alns,out):
+    """resolvers pirmer quality masking for a set of alignments of a read pair
+
+    Args:
+        alns ([pysam.AlignedSegments]) List of alignments from a read pair
+        out (FileHandle): Writeable file handle for output
+    """
+    # determine primer boundaries (outermost aligned base after clipping)
+    reference_start, reference_end = None, None
+    for aln in alns:
+        if not reference_start or aln.reference_start < reference_start:
+            reference_start = aln.reference_start
+        if not reference_end or aln.reference_end > reference_end:
+            reference_end = aln.reference_end
+    # mask newly softclipped bases in all alignedSegments outside of those boundaries
+    for aln in alns:
+        # mask and write
+        out.write(mask_sequence(aln, reference_start, reference_end))
 
 
 def clip_primers(args):
@@ -57,14 +81,16 @@ def clip_primers(args):
         '--strand', '--no-excluded', '--both-ends']
     if args.rejects:
         cmd += ['--rejects-file', args.rejects,]
-    cmd += ['-b', args.bedfile, args.bamfile]
+    cmd += ['-b', args.bedfile, args.bamfile, '|', 'samtools', 'sort', '-n']
 
     # run samtools and capture output
+    sys.stderr.write('Running ampliconclip...\n')
     p = subprocess.Popen(' '.join(cmd), stdout=subprocess.PIPE, shell=True)
-    alignments = pysam.Samfile(p.stdout, "rb")
-    
+    alignments = pysam.AlignmentFile(p.stdout, "rb")
+
     # iterare over alignment and write output
     outfile = bamIO(args.outfile, 'w', True, alignments)
+    buffer = []
     for i, aln in enumerate(alignments):
         # progressmeter
         if i % 10000 == 0:
@@ -73,16 +99,25 @@ def clip_primers(args):
             else:
                 sys.stderr.write('.')
             sys.stderr.flush()
-        # mask and write
-        outfile.write(mask_sequence(aln))
+
+        # group alignments by read pair
+        if not buffer or buffer[-1].qname == aln.qname:
+            buffer.append(aln)
+        else:
+            # resolve
+            resolve(buffer, outfile)
+            # empty buffer
+            buffer = [] 
+    # resolve last reads
+    resolve(buffer, outfile)
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('bedfile', metavar='BED', help='BED file of primer locations (with strand)')
     parser.add_argument('bamfile', metavar='BAM', help='BAM input file')
-    parser.add_argument('-o','--outfile', metavar='FILE', help='outputfile (Default: SAM to STDOUT)', type=str)
-    parser.add_argument('-r','--rejects', metavar='FILE', help='Rejected reads (optional)', type=str)
+    parser.add_argument('-o','--outfile', metavar='FILE', help='outputfile (STDOUT)')
+    parser.add_argument('-r','--rejects', metavar='FILE', help='Rejected reads (optional)')
     args = parser.parse_args()
 
     clip_primers(args)
